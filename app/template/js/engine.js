34e85fb4000732c75be763de299f09686b3f6444
x404 <kronos2003@gmail.com>
2020-03-03 12:45:59 +0200

    Добавлены всплывающие подсказки для машины на главной

--- app/template/js/engine.js
+++ app/template/js/engine.js
@@ -1,40 +1,35 @@
 $(document).ready(function(){
 
 
-	// mobile-menu
-	$('#navbar').each(function(){
-		var $this = $(this),
-			$link = $('.navbar-toggle'),
-			$close = $('.close-menu'),
+	// // mobile-menu
+	// $('#navbar').each(function(){
+	// 	var $this = $(this),
+	// 		$link = $('.navbar-toggle'),
+	// 		$close = $('.close-menu'),
 
-			init = function(){
-				$link.on('click', openMenu);
-				$close.on('click', closeMenu);
-			},
-			openMenu = function(e){
-				e.preventDefault();
-				$('body').addClass('o-menu');
+	// 		init = function(){
+	// 			$link.on('click', openMenu);
+	// 			$close.on('click', closeMenu);
+	// 		},
+	// 		openMenu = function(e){
+	// 			e.preventDefault();
+	// 			$('body').addClass('o-menu');
 
-			},
-			closeMenu = function(e){
-				e.preventDefault();
-				$('body').removeClass('o-menu');
-			};
-		init();
-	});	
-});
+	// 		},
+	// 		closeMenu = function(e){
+	// 			e.preventDefault();
+	// 			$('body').removeClass('o-menu');
+	// 		};
+	// 	init();
+	// });	
 
 
-$(function(){
-	$('.policy input').click(function(){
-		var $this = $(this),
-			$submit = $this.closest('.form-policy');
+	$(".homepage .catalog_link").mouseenter(function() {
+		const cat = $(this).data('cat');
+		$('[data-cat="' + cat + '"]').addClass('active');
+	}).mouseleave(function() {
+		const cat = $(this).data('cat');
+		$('[data-cat="' + cat + '"]').removeClass('active');
+	});
 
-		if ($this.is(':checked')){
-			$submit.find('.input, .form-control, .submit, .btn-submit, textarea, input[type=radio], button').removeAttr('disabled');
-		} else {
-			$submit.addClass('disabled');
-			$submit.find('.input, .form-control, .submit, .btn-submit, textarea, input[type=radio], button').attr('disabled', true);
-		}
-	})
 });
