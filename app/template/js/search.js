1d0ae7644d95d1e53c7451d6bffd84092b4fad77
x404 <kronos2003@gmail.com>
2020-03-13 22:09:00 +0200

    Добавлены стилизоыванные выпадающие списки для вкладки Application на странице CAtalogue Search

--- app/template/js/search.js
+++ app/template/js/search.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$("#ss_type_id-3").multipleSelect({
+	$("#ss_type_id").multipleSelect({
 		single: true,
 		filter: true,
 		placeholder : '',
@@ -18,5 +18,58 @@ typelist = new PerfectScrollbar('#productsizemenu .listwrapper', {
 				minScrollbarLength: 100,
 				maxScrollbarLength: 100,
 			});
+
+
+
+	$('#selBrand').multipleSelect({
+		single: true,  
+		filter: true ,
+		onOpen: function() {  
+			// $('#brand_tab .ms-parent .ms-drop .ms-search input').focus();
+			// $("#brand_tab ul").getNiceScroll()[0].show().onResize();
+		}
+	});
+
+	$('#selClass').multipleSelect({
+		filter: true ,
+		onOpen: function() {  
+			// $('#class_tab .ms-parent .ms-drop .ms-search input').focus();
+			// $("#class_tab ul").getNiceScroll()[0].show().onResize();
+		}
+	});
+
+	$('#selModel').multipleSelect({
+		filter: true,
+		selectAll: false,
+		onOpen: function() {  
+			// $('#model_tab .ms-parent .ms-drop .ms-search input').focus();
+			// $("#model_tab ul").getNiceScroll()[0].show().onResize();
+		}
+	});
+
+	$('#selEngine').multipleSelect({
+		filter: true,
+		onOpen: function() {
+			// $("#engine_tab ul").getNiceScroll()[0].show().onResize();
+		}  
+	});  
+
+
+	$('#selYear').multipleSelect({
+		filter: true,
+		onOpen: function() {
+			// $("#year_tab ul").getNiceScroll()[0].show().onResize();
+		}
+	});
+
+	$('#selBody').multipleSelect({
+		filter: true ,
+		onOpen: function() {
+			// $("#body_tab ul").getNiceScroll()[0].show().onResize();
+		}
+	}); 
+
+
+
 })
 
